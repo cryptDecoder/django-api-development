f64c2c5776b88984091a89501d94f58a38a36784
Pruthvirajs2 <pruthvirajs2007@gmail.com>
2020-10-04 20:51:06 +0530

    User operation app added

--- Teamline/settings.py
+++ Teamline/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -28,8 +27,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'user_operations.apps.UserOperationsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
